056eb74c9f46904219dd2e94214db8bd11120c3f
juanignacio87 <97361082+juanignacio87@users.noreply.github.com>
2023-03-11 18:47:51 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Ejercicio 1
+Console.WriteLine("Ingrese su nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Su nombre es " + nombre);
+
+Console.ReadKey();
+Console.Clear();
+
+//Ejercicio 2
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+
+Console.WriteLine(datetime);
